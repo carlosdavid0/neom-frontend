9dadb3095ca9ca52bd730b723e1eed1984c13e57
carlos David <romao.carlosdavi@gmail.com>
2024-11-02 18:25:22 -0300

    feat: restart project using vite
    use this way because it's better migrate pieces of code than using current codebase.

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+    return (
+        <div className="flex items-center justify-center min-h-screen bg-gray-100">
+            <div className="bg-white p-8 rounded shadow-md text-center">
+                <h1 className="text-2xl font-bold mb-4">Welcome to Neom Frontend</h1>
+                <p className="text-gray-700">This is your initial screen setup using Tailwind CSS.</p>
+            </div>
+        </div>
+    );
+}
+
+export default App;
